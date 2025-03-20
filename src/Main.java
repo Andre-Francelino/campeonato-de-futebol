76e312b05417c3f322b4f17aba43ccccea787af5
Andre Francelino <andrefgomes.github.version@gmail.com>
2025-03-19 21:56:25 -0300

    update class Main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
+    public static void main(String[] args) {
+        Atleta atleta1 = new Atleta("Marcão", 22, "Centro-avante");
+        Time time1 = new Time("Jardim Pereba Futebol Clube", "Charles Bryson");
 
-
+        System.out.println(atleta1.toString());
+        System.out.println(time1.toString());
+    }
 
 }
